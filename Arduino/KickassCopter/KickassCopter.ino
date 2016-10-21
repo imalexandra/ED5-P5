47503263c5b5ecd4b80345ca2ff1ba8687735883
Andrius <kulsinskas.andrius@gmail.com>
2016-10-21 17:20:13 +0200

    code + tried to write about escs

--- Arduino/KickassCopter/KickassCopter.ino
+++ Arduino/KickassCopter/KickassCopter.ino
@@ -1,92 +1,82 @@
-//#include <math.h>
-//#include <stdio.h>
-//#include <stdarg.h> 
+#include <Servo.h> //servo library
 
-//#include <AP_MOTORS.h>
-//#include <Arduino.h>
-
-#include <Servo.h>
-
-#define MAX_SIGNAL 2000
-#define MIN_SIGNAL 700
-#define MOTOR_1_PIN 12
+#define MAX_SIGNAL 2000 //max pwm signal, need to recalibrate ESCs if changed
+#define MIN_SIGNAL 700 //min pwm signal, "-"
+#define OUT_SIGNAL 850 //pwm signal we'll be sending out to motors during the rest of the program
+#define MOTOR_1_PIN 12 //motor pins, APM board has weird layout
 #define MOTOR_2_PIN 11
 #define MOTOR_3_PIN 8
 #define MOTOR_4_PIN 7
 
-Servo motor1;
+Servo motor1; //define servo variables for motors
 Servo motor2;
 Servo motor3;
 Servo motor4;
 
+long prevTimer = 0; //initial timer
+long currTimer = 100; //initial timer
+const long initTime = 4000; //4 seconds to turn on ESCs
+const long runTime = 10000; //10 seconds to run program
+bool escCalibrate = false; //have we calibrated ESCs yet?
+bool programStarted = false;
+
 void setup() {
-//    digitalWrite(12,HIGH);  // pulling high before changing to output avoids a momentary drop of the pin to low because the ESCs have a pull-down resistor it seems
-//    digitalWrite(11,HIGH);
-//    pinMode(12,OUTPUT); // OUT1 (PB6/OC1B)
-//    pinMode(11,OUTPUT); // OUT2 (PB5/OC1A)
-//    digitalWrite(12,HIGH);  // pulling high before changing to output avoids a momentary drop of the pin to low because the ESCs have a pull-down resistor it seems
-//    digitalWrite(11,HIGH);
-//    digitalWrite(8,HIGH);
-//    digitalWrite(7,HIGH);
-//    digitalWrite(6,HIGH);
-//    pinMode(8,OUTPUT); // OUT3 (PH5/OC4C)
-//    pinMode(7,OUTPUT); // OUT4 (PH4/OC4B)
-//    pinMode(6,OUTPUT); // OUT5 (PH3/OC4A)
-//    digitalWrite(8,HIGH);
-//    digitalWrite(7,HIGH);
-//    digitalWrite(6,HIGH);
-//    digitalWrite(3,HIGH);
-//    digitalWrite(2,HIGH);
-//    digitalWrite(5,HIGH);
-//    pinMode(3,OUTPUT); // OUT6 (PE5/OC3C)
-//    pinMode(2,OUTPUT); // OUT7 (PE4/OC3B)
-//    pinMode(5,OUTPUT); // OUT8 (PE3/OC3A)
-//    digitalWrite(3,HIGH);
-//    digitalWrite(2,HIGH);
-//    digitalWrite(5,HIGH);
-//    digitalWrite(45,HIGH);
-//    digitalWrite(44,HIGH);
-//    pinMode(45, OUTPUT); // OUT10 (PL4/OC5B)
-//    pinMode(44, OUTPUT); // OUT11 (PL5/OC5C)
-//    digitalWrite(45,HIGH);
-//    digitalWrite(44,HIGH);
-
     Serial.begin(9600);
-    Serial.println("Program begin...");
-    Serial.println("This program will calibrate the ESC.");
-
-    motor1.attach(MOTOR_1_PIN);
+    Serial.println("Program begins...");
+    
+    motor1.attach(MOTOR_1_PIN); //connect servo variables to pins
     motor2.attach(MOTOR_2_PIN);
     motor3.attach(MOTOR_3_PIN);
-//    motor4.attach(MOTOR_4_PIN);
-
-    Serial.println("Now writing maximum output.");
-    Serial.println("Turn on power source, then wait 2 seconds and press any key.");
-    motor1.writeMicroseconds(MAX_SIGNAL);
-    motor2.writeMicroseconds(MAX_SIGNAL);
-    motor3.writeMicroseconds(MAX_SIGNAL);
-//    motor4.writeMicroseconds(MAX_SIGNAL);
-
-    while (!Serial.available());
-    Serial.read();
-
-    Serial.println("Sending minimum output");
-    motor1.writeMicroseconds(MIN_SIGNAL);
-    motor2.writeMicroseconds(MIN_SIGNAL);
-    motor3.writeMicroseconds(MIN_SIGNAL);
-//    motor4.writeMicroseconds(MIN_SIGNAL);
-
-    while (!Serial.available());
-    Serial.read();
-
-    motor1.writeMicroseconds(820);
-    motor2.writeMicroseconds(820);
-    motor3.writeMicroseconds(820);
-//    motor4.writeMicroseconds(820);
+    motor4.attach(MOTOR_4_PIN);
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+    long currTimer = millis(); //get time
+    Serial.println(currTimer);
+//  Only need this part if calibrating ESCs. The ones used in our prototype
+//  need only min signal on startup to run
+//    Serial.println("Now writing maximum output.");
+//    Serial.println("Turn on power source, then wait 2 seconds and press any key.");
+//    motor1.writeMicroseconds(MAX_SIGNAL);
+//    motor2.writeMicroseconds(MAX_SIGNAL);
+//    motor3.writeMicroseconds(MAX_SIGNAL);
+//    motor4.writeMicroseconds(MAX_SIGNAL);
+//
+    if(programStarted == false){
+      Serial.println("Waiting for input.");
+      while (!Serial.available());
+      Serial.read();
+      programStarted = true;
+      prevTimer = millis();
+      Serial.println("Program will now start");
+      Serial.println(currTimer);
+      Serial.println(prevTimer);
+    }
+    if(programStarted == true){
+      //Check if we're < 4 seconds and ESCs havent been calibrated yet
+      if ((currTimer - prevTimer <= initTime) && (escCalibrate == false)) {
+        motor1.writeMicroseconds(MIN_SIGNAL); //send out calib. signal
+        motor2.writeMicroseconds(MIN_SIGNAL);
+        motor3.writeMicroseconds(MIN_SIGNAL);
+        motor4.writeMicroseconds(MIN_SIGNAL);
+      }else if((currTimer - prevTimer >= initTime) && (escCalibrate == false)){
+        escCalibrate = true; //otherwise, say that we calibrated
+        Serial.println("ESCs calibrated");
+        prevTimer = currTimer; //reset timer and never repeat this step again
+      }
+      if(escCalibrate == true){ //probably the rest of the program will be under here
+          if(currTimer - prevTimer <= runTime){ //check if < 10 seconds
+            motor1.writeMicroseconds(OUT_SIGNAL); // let's fly!
+            motor2.writeMicroseconds(OUT_SIGNAL);
+            motor3.writeMicroseconds(OUT_SIGNAL);
+            motor4.writeMicroseconds(OUT_SIGNAL);
+          }else{
+            motor1.writeMicroseconds(MIN_SIGNAL); //program ends, time to stop
+            motor2.writeMicroseconds(MIN_SIGNAL);
+            motor3.writeMicroseconds(MIN_SIGNAL);
+            motor4.writeMicroseconds(MIN_SIGNAL);
+          }
+      }
+    }
 }
