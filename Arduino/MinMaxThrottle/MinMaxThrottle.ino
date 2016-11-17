94800fdb144994b627aa0050cd62d3f4f82676f3
Andrius <kulsinskas.andrius@gmail.com>
2016-11-17 15:05:16 +0100

    whatever

--- Arduino/MinMaxThrottle/MinMaxThrottle.ino
+++ Arduino/MinMaxThrottle/MinMaxThrottle.ino
@@ -3,7 +3,7 @@
 #define MAX_SIGNAL 2000
 #define MIN_SIGNAL 700
 #define MOTOR_PIN1 12 //pin 1
-//#define MOTOR_PIN2 11 //pin 1
+#define MOTOR_PIN2 11 //pin 1
 
 Servo motor1;
 Servo motor2;
@@ -13,12 +13,12 @@ void setup() {
   Serial.println("This program will calibrate the ESC.");
   //pinMode(12, OUTPUT);
   motor1.attach(MOTOR_PIN1);
-  //motor2.attach(MOTOR_PIN2);
+  motor2.attach(MOTOR_PIN2);
   
   Serial.println("Now writing maximum output.");
   Serial.println("Turn on power source, then wait 2 seconds and press any key.");
   motor1.writeMicroseconds(MAX_SIGNAL);
-  //motor2.writeMicroseconds(MAX_SIGNAL);
+  motor2.writeMicroseconds(MAX_SIGNAL);
   //analogWrite(12, 255);
   // Wait for input
   while (!Serial.available());
@@ -27,61 +27,71 @@ void setup() {
   // Send min output
   Serial.println("Sending minimum output");
   motor1.writeMicroseconds(MIN_SIGNAL);
-  //motor2.writeMicroseconds(MIN_SIGNAL);
+  motor2.writeMicroseconds(MIN_SIGNAL);
   
   Serial.println("Calibrated");
   while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(700);
-  Serial.println("700");
+  motor1.writeMicroseconds(800);
+  Serial.println("800");
   while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(830);
-Serial.println("830");
+  motor1.writeMicroseconds(900);
+Serial.println("900");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(960);
-Serial.println("960");
+  motor1.writeMicroseconds(1000);
+Serial.println("1000");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1090);
-    Serial.println("1090");
+  motor1.writeMicroseconds(1100);
+    Serial.println("1100");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1220);
-  Serial.println("1220");
+  motor1.writeMicroseconds(1200);
+  Serial.println("1200");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1350);
-  Serial.println("1350");
+  motor1.writeMicroseconds(1300);
+  Serial.println("1300");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1480);
+  motor1.writeMicroseconds(1400);
 //motor2.writeMicroseconds(1200);
-Serial.println("1480");
+Serial.println("1400");
   while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1610);
-Serial.println("1610");
+  motor1.writeMicroseconds(1500);
+Serial.println("1500");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1740);
-Serial.println("1740");
+  motor1.writeMicroseconds(1600);
+Serial.println("1600");
     while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
-  motor1.writeMicroseconds(1870);
-Serial.println("1870");
+  motor1.writeMicroseconds(1700);
+Serial.println("1700");
     while (!Serial.available());
+  Serial.read();
+  //analogWrite(12, 255);
+  motor1.writeMicroseconds(1800);
+Serial.println("1800");
+ while (!Serial.available());
+  Serial.read();
+  //analogWrite(12, 255);
+  motor1.writeMicroseconds(1900);
+Serial.println("1900");
+ while (!Serial.available());
   Serial.read();
   //analogWrite(12, 255);
   motor1.writeMicroseconds(2000);
