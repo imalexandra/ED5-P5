a593119aa55f75ded0ba1d29fec6146cac05234b
Andrius <kulsinskas.andrius@gmail.com>
2016-11-23 12:18:56 +0100

    updated codes

--- report/Arduino/KickassCopter/KickassCopter.ino
+++ report/Arduino/KickassCopter/KickassCopter.ino
@@ -1,14 +1,50 @@
 #include <Servo.h> //servo library
 #include <Wire.h> //I2C library
+#include <NewPing.h>
+#include <SPI.h>
+#include <math.h>
 
 #define MAX_SIGNAL 2000 //max pwm signal, need to recalibrate ESCs if changed
 #define MIN_SIGNAL 700 //min pwm signal, "-"
 #define OUT_SIGNAL 850 //pwm signal we'll be sending out to motors during the rest of the program
+
 #define MOTOR_1_PIN 12 //motor pins, APM board has weird layout
 #define MOTOR_2_PIN 11
 #define MOTOR_3_PIN 8
 #define MOTOR_4_PIN 7
-#define I2C_ADDRESS 0x77 //77?
+
+#define TRIGGER_PIN1  A0
+#define ECHO_PIN1     A1
+#define TRIGGER_PIN2  A2
+#define ECHO_PIN2     A3
+#define TRIGGER_PIN3  A4
+#define ECHO_PIN3     A5
+#define TRIGGER_PIN4  A6
+#define ECHO_PIN4     A7
+#define MAX_DISTANCE 200
+
+#define ToD(x) (x/131)
+#define ToG(x) (x*9.80665/16384)
+#define Aoffset 0.8 
+
+#define xAxis 0
+#define yAxis 1
+#define zAxis 2
+
+int time=0;
+int time_old=0;
+
+const int ChipSelPin1 = 53;
+
+float angle=0;
+float angleX=0;
+float angleY=0;
+float angleZ=0;
+
+NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
+NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
+NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE);
+NewPing sonar4(TRIGGER_PIN4, ECHO_PIN4, MAX_DISTANCE);
 
 Servo motor1; //define servo variables for motors
 Servo motor2;
@@ -21,34 +57,26 @@ const long initTime = 4000; //4 seconds to turn on ESCs
 const long runTime = 10000; //10 seconds to run program
 bool escCalibrate = false; //have we calibrated ESCs yet?
 bool programStarted = false; //check when connected to PC over USB
-const unsigned char oversampling_setting = 3; //oversampling for measurement
-const unsigned char pressure_waittime[4] = { 5, 8, 14, 26 };
-
-//just taken from the BMP085 datasheet, some variables for calcing temp/pressure
-int ac1;
-int ac2;
-int ac3;
-unsigned int ac4;
-unsigned int ac5;
-unsigned int ac6;
-int b1;
-int b2;
-int mb;
-int mc;
-int md;
 
 void setup() {
-    Serial.begin(9600);
+    Serial.begin(115200);
     Serial.println("Program begins...");
     Wire.begin();
-    bmp085_get_cal_data(); //callibrate??
     motor1.attach(MOTOR_1_PIN); //connect servo variables to pins
     motor2.attach(MOTOR_2_PIN);
     motor3.attach(MOTOR_3_PIN);
     motor4.attach(MOTOR_4_PIN);
-
+    //As per APM standard code, stop the barometer from holding the SPI bus
+    pinMode(40, OUTPUT);
+    digitalWrite(40, HIGH);
+    SPI.begin();  
+    SPI.setClockDivider(SPI_CLOCK_DIV16); 
+    SPI.setBitOrder(MSBFIRST); 
+    SPI.setDataMode(SPI_MODE0);
+    delay(100);
+    pinMode(ChipSelPin1, OUTPUT);
+    ConfigureMPU6000();  // configure chip 
 }
-void bmp085_read_temperature_and_pressure(int& temperature, long& pressure);
 void loop() {
     long currTimer = millis(); //get time
 //  Only need this part if calibrating ESCs. The ones used in our prototype
@@ -87,13 +115,36 @@ void loop() {
             motor2.writeMicroseconds(OUT_SIGNAL);
             motor3.writeMicroseconds(OUT_SIGNAL);
             motor4.writeMicroseconds(OUT_SIGNAL);
-            int  temperature = 0; //zero temp/pressure
-            long pressure = 0;
-            bmp085_read_temperature_and_pressure(&temperature,&pressure); //read them
-            Serial.print(temperature,DEC); //print them
-            Serial.print(" ");
-            Serial.print(pressure,DEC);
-            Serial.println();
+            delay(100);
+            Serial.print("Ping1: ");
+            Serial.print(sonar1.ping_cm());
+            Serial.println("cm");
+            //Serial.print("Ping2: ");
+            //Serial.print(sonar2.ping_cm());
+            //Serial.println("cm");
+            //Serial.print("Ping3: ");
+            //Serial.print(sonar3.ping_cm());
+            //Serial.println("cm");
+            //Serial.print("Ping4: ");
+            //Serial.print(sonar4.ping_cm());
+            //Serial.println("cm");
+            Serial.print("Acc X ");
+            Serial.print(AcceX(ChipSelPin1));
+            Serial.print("   ");
+            Serial.print("Acc Y ");
+            Serial.print(AcceY(ChipSelPin1));
+            Serial.print("   ");
+            Serial.print("Acc Z ");  
+            Serial.print(AcceZ(ChipSelPin1));
+            Serial.print("   ");  
+            Serial.print("Gyro X ");  
+            Serial.print(GyroX(ChipSelPin1));
+            Serial.print("   ");  
+            Serial.print("Gyro Y ");  
+            Serial.print(GyroY(ChipSelPin1)); 
+            Serial.print("   "); 
+            Serial.print("Gyro Z ");  
+            Serial.println(GyroZ(ChipSelPin1));  
           }else{
             motor1.writeMicroseconds(MIN_SIGNAL); //program ends, time to stop
             motor2.writeMicroseconds(MIN_SIGNAL);
@@ -101,147 +152,162 @@ void loop() {
             motor4.writeMicroseconds(MIN_SIGNAL);
           }
       }
-    }
+   }
 }
 
-void bmp085_read_temperature_and_pressure(int* temperature, long* pressure) { //reading the temp/pressure
-  int  ut= bmp085_read_ut(); //get temperature
-  long up = bmp085_read_up(); //and pressure
-  long x1, x2, x3, b3, b5, b6, p; //define some variables for calculations
-  unsigned long b4, b7;
-
-  //calculate the temperature
-  x1 = ((long)ut - ac6) * ac5 >> 15;
-  x2 = ((long) mc << 11) / (x1 + md);
-  b5 = x1 + x2;
-  *temperature = (b5 + 8) >> 4;
-   
-  //calculate the pressure
-  b6 = b5 - 4000;
-  x1 = (b2 * (b6 * b6 >> 12)) >> 11; 
-  x2 = ac2 * b6 >> 11;
-  x3 = x1 + x2;
-  b3 = (((int32_t) ac1 * 4 + x3)<<oversampling_setting + 2) >> 2;
-  x1 = ac3 * b6 >> 13;
-  x2 = (b1 * (b6 * b6 >> 12)) >> 16;
-  x3 = ((x1 + x2) + 2) >> 2;
-  b4 = (ac4 * (uint32_t) (x3 + 32768)) >> 15;
-  b7 = ((uint32_t) up - b3) * (50000 >> oversampling_setting);
-  p = b7 < 0x80000000 ? (b7 * 2) / b4 : (b7 / b4) * 2;
-  
-  x1 = (p >> 8) * (p >> 8);
-  x1 = (x1 * 3038) >> 16;
-  x2 = (-7357 * p) >> 16;
-  *pressure = p + ((x1 + x2 + 3791) >> 4);
-}
-
-unsigned int bmp085_read_ut() { //read temperature
-  write_register(0xf4,0x2e); //call the barometer with some addresses
-  delay(5); //longer than 4.5 ms
-  return read_int_register(0xf6); //return temperature value
-}
-
-void  bmp085_get_cal_data() { //calibarting
-  Serial.println("Reading Calibration Data");
-  ac1 = read_int_register(0xAA);
-  Serial.print("AC1: ");
-  Serial.println(ac1,DEC);
-  ac2 = read_int_register(0xAC);
-  Serial.print("AC2: ");
-  Serial.println(ac2,DEC);
-  ac3 = read_int_register(0xAE);
-  Serial.print("AC3: ");
-  Serial.println(ac3,DEC);
-  ac4 = read_int_register(0xB0);
-  Serial.print("AC4: ");
-  Serial.println(ac4,DEC);
-  ac5 = read_int_register(0xB2);
-  Serial.print("AC5: ");
-  Serial.println(ac5,DEC);
-  ac6 = read_int_register(0xB4);
-  Serial.print("AC6: ");
-  Serial.println(ac6,DEC);
-  b1 = read_int_register(0xB6);
-  Serial.print("B1: ");
-  Serial.println(b1,DEC);
-  b2 = read_int_register(0xB8);
-  Serial.print("B2: ");
-  Serial.println(b1,DEC);
-  mb = read_int_register(0xBA);
-  Serial.print("MB: ");
-  Serial.println(mb,DEC);
-  mc = read_int_register(0xBC);
-  Serial.print("MC: ");
-  Serial.println(mc,DEC);
-  md = read_int_register(0xBE);
-  Serial.print("MD: ");
-  Serial.println(md,DEC);
+void SPIwrite(byte reg, byte data, int ChipSelPin) {
+  uint8_t dump;
+  digitalWrite(ChipSelPin,LOW);
+  dump=SPI.transfer(reg);
+  dump=SPI.transfer(data);
+  digitalWrite(ChipSelPin,HIGH);
 }
 
 
-long bmp085_read_up() { //get pressure from barometer
-  write_register(0xf4,0x34+(oversampling_setting<<6));
-  delay(pressure_waittime[oversampling_setting]);
-  
-  unsigned char msb, lsb, xlsb; //???
-  Wire.beginTransmission(I2C_ADDRESS); //connect to barometer
-  Wire.write(0xf6);  // register to read
-  Wire.endTransmission(); //end communication
+uint8_t SPIread(byte reg,int ChipSelPin) {
+  uint8_t dump;
+  uint8_t return_value;
+  uint8_t addr=reg|0x80;
+  digitalWrite(ChipSelPin,LOW);
+  dump=SPI.transfer(addr);
+  return_value=SPI.transfer(0x00);
+  digitalWrite(ChipSelPin,HIGH);
+  return(return_value);
+}
 
-  Wire.requestFrom(I2C_ADDRESS, 3); // read a byte
-  while(!Wire.available()) { //these while loops could cause problems if something goes wrong
-    // waiting
+
+int AcceX(int ChipSelPin) {
+  uint8_t AcceX_H=SPIread(0x3B,ChipSelPin);
+  uint8_t AcceX_L=SPIread(0x3C,ChipSelPin);
+  int16_t AcceX=AcceX_H<<8|AcceX_L;
+  return(AcceX);
+}
+
+
+int AcceY(int ChipSelPin) {
+  uint8_t AcceY_H=SPIread(0x3D,ChipSelPin);
+  uint8_t AcceY_L=SPIread(0x3E,ChipSelPin);
+  int16_t AcceY=AcceY_H<<8|AcceY_L;
+  return(AcceY);
+}
+
+
+int AcceZ(int ChipSelPin) {
+  uint8_t AcceZ_H=SPIread(0x3F,ChipSelPin);
+  uint8_t AcceZ_L=SPIread(0x40,ChipSelPin);
+  int16_t AcceZ=AcceZ_H<<8|AcceZ_L;
+  return(AcceZ);
+}
+
+
+int GyroX(int ChipSelPin) {
+  uint8_t GyroX_H=SPIread(0x43,ChipSelPin);
+  uint8_t GyroX_L=SPIread(0x44,ChipSelPin);
+  int16_t GyroX=GyroX_H<<8|GyroX_L;
+  return(GyroX);
+}
+
+
+int GyroY(int ChipSelPin) {
+  uint8_t GyroY_H=SPIread(0x45,ChipSelPin);
+  uint8_t GyroY_L=SPIread(0x46,ChipSelPin);
+  int16_t GyroY=GyroY_H<<8|GyroY_L;
+  return(GyroY);
+}
+
+
+int GyroZ(int ChipSelPin) {
+  uint8_t GyroZ_H=SPIread(0x47,ChipSelPin);
+  uint8_t GyroZ_L=SPIread(0x48,ChipSelPin);
+  int16_t GyroZ=GyroZ_H<<8|GyroZ_L;
+  return(GyroZ);
+}
+
+
+//--- Function to obtain angles based on accelerometer readings ---//
+float AcceDeg(int ChipSelPin,int AxisSelect) {
+  float Ax=ToG(AcceX(ChipSelPin));
+  float Ay=ToG(AcceY(ChipSelPin));
+  float Az=ToG(AcceZ(ChipSelPin));
+  float ADegX=((atan(Ax/(sqrt((Ay*Ay)+(Az*Az)))))/PI)*180;
+  float ADegY=((atan(Ay/(sqrt((Ax*Ax)+(Az*Az)))))/PI)*180;
+  float ADegZ=((atan((sqrt((Ax*Ax)+(Ay*Ay)))/Az))/PI)*180;
+  switch (AxisSelect)
+  {
+    case 0:
+    return ADegX;
+    break;
+    case 1:
+    return ADegY;
+    break;
+    case 2:
+    return ADegZ;
+    break;
   }
-  msb = Wire.read();
-  while(!Wire.available()) {
-    // waiting
-  }
-  lsb |= Wire.read();
-  while(!Wire.available()) {
-    // waiting
-  }
-  xlsb |= Wire.read();
-  return (((long)msb<<16) | ((long)lsb<<8) | ((long)xlsb)) >>(8-oversampling_setting); //return these stored into 1 variable
 }
 
-void write_register(unsigned char r, unsigned char v) //write something to barometer
+
+//--- Function to obtain angles based on gyroscope readings ---//
+float GyroDeg(int ChipSelPin, int AxisSelect) {
+  time_old=time;
+  time=millis();
+  float dt=time-time_old;
+  if (dt>=1000)
+  {
+    dt=0;
+  }
+  float Gx=ToD(GyroX(ChipSelPin));
+  if (Gx>0 && Gx<1.4)
+  {
+    Gx=0;
+  }
+  float Gy=ToD(GyroY(ChipSelPin));
+  float Gz=ToD(GyroZ(ChipSelPin));
+  angleX+=Gx*(dt/1000);
+  angleY+=Gy*(dt/1000);
+  angleZ+=Gz*(dt/1000);
+  switch (AxisSelect)
+  {
+    case 0:
+    return angleX;
+    break;
+    case 1:
+    return angleY;
+    break;
+    case 2:
+    return angleZ;
+    break;
+  }
+}
+
+
+void ConfigureMPU6000()
 {
-  Wire.beginTransmission(I2C_ADDRESS);
-  Wire.write(r);
-  Wire.write(v);
-  Wire.endTransmission();
-}
+  // DEVICE_RESET @ PWR_MGMT_1, reset device
+  SPIwrite(0x6B,0x80,ChipSelPin1);
+  delay(150);
 
-char read_register(unsigned char r) //read value from barometer
-{
-  unsigned char v;
-  Wire.beginTransmission(I2C_ADDRESS);
-  Wire.write(r);  // register to read
-  Wire.endTransmission();
+  // TEMP_DIS @ PWR_MGMT_1, wake device and select GyroZ clock
+  SPIwrite(0x6B,0x03,ChipSelPin1);
+  delay(150);
 
-  Wire.requestFrom(I2C_ADDRESS, 1); // read a byte
-  while(!Wire.available()) {
-    // waiting
-  }
-  v = Wire.read();
-  return v;
-}
+  // I2C_IF_DIS @ USER_CTRL, disable I2C interface
+  SPIwrite(0x6A,0x10,ChipSelPin1);
+  delay(150);
 
-int read_int_register(unsigned char r)
-{
-  unsigned char msb, lsb;
-  Wire.beginTransmission(I2C_ADDRESS);
-  Wire.write(r);  // register to read
-  Wire.endTransmission();
+  // SMPRT_DIV @ SMPRT_DIV, sample rate at 1000Hz
+  SPIwrite(0x19,0x00,ChipSelPin1);
+  delay(150);
 
-  Wire.requestFrom(I2C_ADDRESS, 2); // read a byte
-  while(!Wire.available()) {
-    // waiting
-  }
-  msb = Wire.read();
-  while(!Wire.available()) {
-    // waiting
-  }
-  lsb = Wire.read();
-  return (((int)msb<<8) | ((int)lsb));
+  // DLPF_CFG @ CONFIG, digital low pass filter at 42Hz
+  SPIwrite(0x1A,0x03,ChipSelPin1);
+  delay(150);
+
+  // FS_SEL @ GYRO_CONFIG, gyro scale at 250dps
+  SPIwrite(0x1B,0x00,ChipSelPin1);
+  delay(150);
+
+  // AFS_SEL @ ACCEL_CONFIG, accel scale at 2g (1g=8192)
+  SPIwrite(0x1C,0x00,ChipSelPin1);
+  delay(150);
 }
--- report/Arduino/MinMaxThrottle/MinMaxThrottle.ino
+++ report/Arduino/MinMaxThrottle/MinMaxThrottle.ino
@@ -4,11 +4,15 @@
 #define MIN_SIGNAL 700
 #define MOTOR_PIN1 12 //pin 1
 #define MOTOR_PIN2 11 //pin 1
+#define MOTOR_PIN3 8 //pin 1
+#define MOTOR_PIN4 7 //pin 1
 int loopVar = 0;
 int incomingByte = MIN_SIGNAL;
 
 Servo motor1;
 Servo motor2;
+Servo motor3;
+Servo motor4;
 void setup() {
   Serial.begin(9600);
   Serial.setTimeout(500) ;
@@ -16,17 +20,23 @@ void setup() {
   Serial.println("This program will calibrate the ESC.");
   motor1.attach(MOTOR_PIN1);
   motor2.attach(MOTOR_PIN2);
+  motor3.attach(MOTOR_PIN3);
+  motor4.attach(MOTOR_PIN4);
   
   Serial.println("Now writing maximum output.");
   Serial.println("Turn on power source, then wait 2 seconds and press any key.");
-  //motor1.writeMicroseconds(MAX_SIGNAL);
-  //motor2.writeMicroseconds(MAX_SIGNAL);
-  //while (!Serial.available());
-  //Serial.read();
+  motor1.writeMicroseconds(MAX_SIGNAL);
+  motor2.writeMicroseconds(MAX_SIGNAL);
+  motor3.writeMicroseconds(MAX_SIGNAL);
+  motor4.writeMicroseconds(MAX_SIGNAL);
+  while (!Serial.available());
+  Serial.read();
 
   Serial.println("Sending minimum output");
   motor1.writeMicroseconds(MIN_SIGNAL);
   motor2.writeMicroseconds(MIN_SIGNAL);
+  motor3.writeMicroseconds(MIN_SIGNAL);
+  motor4.writeMicroseconds(MIN_SIGNAL);
   while (!Serial.available());
   Serial.read();
   Serial.println("Calibrated");
@@ -35,6 +45,9 @@ void setup() {
     while (!Serial.available());
     incomingByte = Serial.parseInt();
     motor1.writeMicroseconds(incomingByte);
+    motor2.writeMicroseconds(incomingByte);
+    motor3.writeMicroseconds(incomingByte);
+    motor4.writeMicroseconds(incomingByte);
     Serial.println(incomingByte);
   }
 }
