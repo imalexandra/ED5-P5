ef1bcf35cb85d7af4b14fcf7c9d94b9ec1fb2597
imalexandra <torok.alexandra@yahoo.com>
2016-11-26 03:51:25 +0100

    separate ino file for acc and gyro, modification to main ino file

--- report/Arduino/acc_gyro_only/acc_gyro_only.ino
+++ report/Arduino/acc_gyro_only/acc_gyro_only.ino
@@ -1,27 +1,6 @@
-#include <Servo.h> //servo library
 #include <Wire.h> //I2C library
-#include <NewPing.h>
 #include <SPI.h>
 #include <math.h>
-
-#define MAX_SIGNAL 2000 //max pwm signal, need to recalibrate ESCs if changed
-#define MIN_SIGNAL 700 //min pwm signal, "-"
-#define OUT_SIGNAL 850 //pwm signal we'll be sending out to motors during the rest of the program
-
-#define MOTOR_1_PIN 12 //motor pins, APM board has weird layout
-#define MOTOR_2_PIN 11
-#define MOTOR_3_PIN 8
-#define MOTOR_4_PIN 7
-
-#define TRIGGER_PIN1  A0
-#define ECHO_PIN1     A1
-#define TRIGGER_PIN2  A2
-#define ECHO_PIN2     A3
-#define TRIGGER_PIN3  A4
-#define ECHO_PIN3     A5
-#define TRIGGER_PIN4  A6
-#define ECHO_PIN4     A7
-#define MAX_DISTANCE 200
 
 #define ToD(x) (x/131) //gyro 131-LSB Sensitivity
 #define ToG(x) (x*9.9000/16384) //acc  16384-LSB Sensitivity
@@ -41,31 +20,16 @@ float angleX=0;
 float angleY=0;
 float angleZ=0;
 
-NewPing sonar1(TRIGGER_PIN1, ECHO_PIN1, MAX_DISTANCE);
-NewPing sonar2(TRIGGER_PIN2, ECHO_PIN2, MAX_DISTANCE);
-NewPing sonar3(TRIGGER_PIN3, ECHO_PIN3, MAX_DISTANCE);
-NewPing sonar4(TRIGGER_PIN4, ECHO_PIN4, MAX_DISTANCE);
-
-Servo motor1; //define servo variables for motors
-Servo motor2;
-Servo motor3;
-Servo motor4;
-
 long prevTimer = 0; //initial timer
 long currTimer = 100; //initial timer
-const long initTime = 4000; //4 seconds to turn on ESCs
 const long runTime = 10000; //10 seconds to run program
-bool escCalibrate = false; //have we calibrated ESCs yet?
 bool programStarted = false; //check when connected to PC over USB
 
 void setup() {
+  
     Serial.begin(115200);
     Serial.println("Program begins...");
     Wire.begin();
-    motor1.attach(MOTOR_1_PIN); //connect servo variables to pins
-    motor2.attach(MOTOR_2_PIN);
-    motor3.attach(MOTOR_3_PIN);
-    motor4.attach(MOTOR_4_PIN);
     //As per APM standard code, stop the barometer from holding the SPI bus
     pinMode(40, OUTPUT);
     digitalWrite(40, HIGH);
@@ -77,18 +41,9 @@ void setup() {
     pinMode(ChipSelPin1, OUTPUT);
     ConfigureMPU6000();  // configure chip 
 }
+
 void loop() {
     long currTimer = millis(); //get time
-//  Only need this part if calibrating ESCs. The ones used in our prototype
-//  need only min signal on startup to run
-//    Serial.println("Now writing maximum output.");
-//    Serial.println("Turn on power source, then wait 2 seconds and press any key.");
-//    motor1.writeMicroseconds(MAX_SIGNAL);
-//    motor2.writeMicroseconds(MAX_SIGNAL);
-//    motor3.writeMicroseconds(MAX_SIGNAL);
-//    motor4.writeMicroseconds(MAX_SIGNAL);
-//
-    // Can remove this program once APM is connected to battery too
     if(programStarted == false){
       Serial.println("Waiting for input.");
       while (!Serial.available());
@@ -96,64 +51,26 @@ void loop() {
       programStarted = true;
       prevTimer = millis();
       Serial.println("Program will now start");
-    }
-    if(programStarted == true){
-      //Check if we're < 4 seconds and ESCs havent been calibrated yet
-      if ((currTimer - prevTimer <= initTime) && (escCalibrate == false)) {
-        motor1.writeMicroseconds(MIN_SIGNAL); //send out calib. signal
-        motor2.writeMicroseconds(MIN_SIGNAL);
-        motor3.writeMicroseconds(MIN_SIGNAL);
-        motor4.writeMicroseconds(MIN_SIGNAL);
-      }else if((currTimer - prevTimer >= initTime) && (escCalibrate == false)){
-        escCalibrate = true; //otherwise, say that we calibrated
-        Serial.println("ESCs calibrated");
-        prevTimer = currTimer; //reset timer and never repeat this step again
-      }
-      if(escCalibrate == true){ //probably the rest of the program will be under here
-          if(currTimer - prevTimer <= runTime){ //check if < 10 seconds
-            motor1.writeMicroseconds(OUT_SIGNAL); // let's fly!
-            motor2.writeMicroseconds(OUT_SIGNAL);
-            motor3.writeMicroseconds(OUT_SIGNAL);
-            motor4.writeMicroseconds(OUT_SIGNAL);
-            delay(100);
-            //Serial.print("Ping1: ");
-            //Serial.print(sonar1.ping_cm());
-            //Serial.println("cm");
-            //Serial.print("Ping2: ");
-            //Serial.print(sonar2.ping_cm());
-            //Serial.println("cm");
-            //Serial.print("Ping3: ");
-            //Serial.print(sonar3.ping_cm());
-            //Serial.println("cm");
-            //Serial.print("Ping4: ");
-            //Serial.print(sonar4.ping_cm());
-            //Serial.println("cm");
+    }else{
             Serial.print("Acc X ");
-            Serial.print(AcceDeg(ChipSelPin1, xAxis));
+            Serial.print(AcceDeg(ChipSelPin1, 0));
             Serial.print("   ");
             Serial.print("Acc Y ");
-            Serial.print(AcceDeg(ChipSelPin1, yAxis));
+            Serial.print(AcceDeg(ChipSelPin1, 1));
             Serial.print("   ");
             Serial.print("Acc Z ");  
-            Serial.print(AcceDeg(ChipSelPin1, zAxis));
+            Serial.print(AcceDeg(ChipSelPin1, 2));
             Serial.print("   ");  
             Serial.print("Gyro X ");  
-            Serial.print(GyroDeg(ChipSelPin1, xAxis));
+            Serial.print(GyroDeg(ChipSelPin1, 0));
             Serial.print("   ");  
             Serial.print("Gyro Y ");  
-            Serial.print(GyroDeg(ChipSelPin1, yAxis)); 
+            Serial.print(GyroDeg(ChipSelPin1, 1)); 
             Serial.print("   "); 
             Serial.print("Gyro Z ");  
-            Serial.println(GyroDeg(ChipSelPin1, zAxis));  
-          }else{
-            motor1.writeMicroseconds(MIN_SIGNAL); //program ends, time to stop
-            motor2.writeMicroseconds(MIN_SIGNAL);
-            motor3.writeMicroseconds(MIN_SIGNAL);
-            motor4.writeMicroseconds(MIN_SIGNAL);
+            Serial.println(GyroDeg(ChipSelPin1, 2));          
           }
       }
-   }
-}
 
 void SPIwrite(byte reg, byte data, int ChipSelPin) {
   uint8_t dump;
@@ -257,10 +174,6 @@ float GyroDeg(int ChipSelPin, int AxisSelect) {
     dt=0;
   }
   float Gx=ToD(GyroX(ChipSelPin));
-  if (Gx>0 && Gx<1.4)
-  {
-    Gx=0;
-  }
   float Gy=1-ToD(GyroY(ChipSelPin));
   float Gz=ToD(GyroZ(ChipSelPin));
   angleX+=Gx*(dt/1000);
